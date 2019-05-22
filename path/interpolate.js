5072330fd5ab626a87f2ad28427e8831e085d680
sanazjamloo <sanaz.jamloo@gmail.com>
2019-05-22 14:18:41 -0700

    add groups

--- path/interpolate.js
+++ path/interpolate.js
@@ -1,24 +1,34 @@
 var dataArray = [{x:5, y:5}, {x:10, y:15}, {x:20, y:7}, {x:30, y:18},{x:40, y:10}];
+var interpolateTypes = [d3.curveLinear, d3.curveNatural, d3.curveStep, d3.curveBasis, d3.curveCardinal, d3.curveBundle];
 
 var svg = d3.select("body").append("svg")
                             .attr("height", "100%").attr("width", "100%");
 
+for (var p = 0; p < 6; p++){
 //Line generator
 //Generators must be on top of the code.
-var line = d3.line()
-                .x(function(d,i){ return d.x*6; })
-                .y(function(d,i){ return d.y*4; })
-                .curve(d3.curveCardinal);
+      var line = d3.line()
+                      .x(function(d,i){ return d.x*6; })
+                      .y(function(d,i){ return d.y*4; })
+                      .curve(interpolateTypes[p]);
 
-svg.append("path")
-      .attr("fill", "none")
-      .attr("stroke", "blue")
-      .attr("d", line(dataArray));
+      var shiftX = p*250;
+      var shiftY = 0;
+      //Creating a group for dots
+      var chartGroup = svg.append("g").attr("class","group"+p).attr("transform", "translate("+shiftX+",0)");
 
-//Adding dots
-svg.selectAll("circle")
-   .data(dataArray)
-   .enter().append("circle")
-              .attr("cx", function(d,i){ return d.x*6; })
-              .attr("cy", function(d,i){ return d.y*4; })
-              .attr("r","2");
+      chartGroup.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "blue")
+            .attr("d", line(dataArray));
+
+      //Adding dots
+      chartGroup.selectAll("circle.grp"+p)
+         .data(dataArray)
+         .enter().append("circle")
+                    .attr("class", function(d,i){ return "grp"+i; })
+                    .attr("cx", function(d,i){ return d.x*6; })
+                    .attr("cy", function(d,i){ return d.y*4; })
+                    .attr("r","2");
+
+}
