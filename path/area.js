21b6ee4bc42a7dbf56de2ddbd2618b640ff60ac3
sanazjamloo <sanaz.jamloo@gmail.com>
2019-05-23 10:50:22 -0700

    add scale

--- path/area.js
+++ path/area.js
@@ -4,9 +4,20 @@ var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008',
 var height = 200;
 var width = 500;
 
+//Adding scale
+var y = d3.scaleLinear()
+            .domain([0,180])
+//The values in the range are inverted because this is for our Y axis.
+            .range([height,0]);
+
+console.log(y(0));
+console.log(y(90));
+console.log(y(180));
+
+
 var area = d3.area()
                 .x(function(d,i){ return i*20; })
                 .y0(height)
-                .y1(function(d){ return height - d; });
+                .y1(function(d){ return y(d); });
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100");
 svg.append("path").attr("d", area(dataArray));
