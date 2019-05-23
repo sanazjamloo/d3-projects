bf747a0f96a9702320d60248c2f9ed69563bab4d
sanazjamloo <sanaz.jamloo@gmail.com>
2019-05-23 11:16:18 -0700

    add axis

--- path/area.js
+++ path/area.js
@@ -4,11 +4,16 @@ var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008',
 var height = 200;
 var width = 500;
 
+
+var margin = {left:50, right:50, top:40, bottom:0};
 //Adding scale
 var y = d3.scaleLinear()
             .domain([0,180])
 //The values in the range are inverted because this is for our Y axis.
             .range([height,0]);
+
+//Declaring axis generator
+var yAxis = d3.axisLeft(y);
 
 console.log(y(0));
 console.log(y(90));
@@ -20,4 +25,8 @@ var area = d3.area()
                 .y0(height)
                 .y1(function(d){ return y(d); });
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100");
-svg.append("path").attr("d", area(dataArray));
+var chartGroup = svg.append("g").attr("transform", "translate("+margin.left+", "+margin.top+")");
+chartGroup.append("path").attr("d", area(dataArray));
+chartGroup.append("g")
+        .attr("class", "axis y")
+        .call(yAxis);
