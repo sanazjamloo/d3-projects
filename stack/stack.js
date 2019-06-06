8b7a4b288738f94fead44b1873f728560d4b7add
sanazjamloo <sanaz.jamloo@gmail.com>
2019-06-06 14:38:00 -0700

    log the xml data

--- stack/stack.js
+++ stack/stack.js
@@ -1,14 +1,16 @@
+var parseDate = d3.timeParse("%Y"); //4 digit date format
+
 //loading the data file into stack.js file
 d3.xml("data2.xml").get(function(error, xml){
 
   xml = [].map.call(xml.querySelectorAll("dat"), function(d){
     return {
-      date: d.getAttribute("id"),
+      date: parseDate(d.getAttribute("id")),
       top: +d.querySelector("top").textContent,
       middle: +d.querySelector("middle").textContent,
-      bottom: +d.querySelector("bottom").textContent,
+      bottom: +d.querySelector("bottom").textContent
 
     };
   })
-
+console.log(xml);
 });
