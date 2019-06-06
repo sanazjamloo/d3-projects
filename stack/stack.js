d2ffb3a3f591289f5ce9f91982f89a531e1f6af7
sanazjamloo <sanaz.jamloo@gmail.com>
2019-06-06 14:29:32 -0700

    load xml file to stacks

--- stack/stack.js
+++ stack/stack.js
@@ -1,1 +1,14 @@
+//loading the data file into stack.js file
+d3.xml("data2.xml").get(function(error, xml){
 
+  xml = [].map.call(xml.querySelectorAll("dat"), function(d){
+    return {
+      date: d.getAttribute("id"),
+      top: +d.querySelector("top").textContent,
+      middle: +d.querySelector("middle").textContent,
+      bottom: +d.querySelector("bottom").textContent,
+
+    };
+  })
+
+});
