210cc131c2792680554be741f668bd4d1326e0cc
sanazjamloo <sanaz.jamloo@gmail.com>
2019-06-06 22:42:37 -0700

    add svg to stack

--- stack/stack.js
+++ stack/stack.js
@@ -3,6 +3,11 @@ var parseDate = d3.timeParse("%Y"); //4 digit date format
 //loading the data file into stack.js file
 d3.xml("data2.xml").get(function(error, xml){
 
+  //building the graph
+  var height = 200;
+  var width = 500;
+  var margin = {left: 50, right: 50, top: 40, bottom:0};
+
   xml = [].map.call(xml.querySelectorAll("dat"), function(d){
     return {
       date: parseDate(d.getAttribute("id")),
@@ -12,5 +17,31 @@ d3.xml("data2.xml").get(function(error, xml){
 
     };
   })
-console.log(xml);
+  var x = d3.scaleTime()
+            .domain(d3.extent(xml, function(d) { return d.date; })) //min and max values of the dataset
+            .range([0, width]);
+  var y = d3.scaleLinear()
+            .domain([0, d3.max(xml, function(d) { return d.top+d.middle+d.bottom; })])
+            .range([height, 0]);
+
+  var categories = ['top', 'middle', 'bottom'];
+  var stack = d3.stack().keys(categories);
+  var area = d3.area ()
+                .x(function (d) { return d.data.date; })
+                .y0(function (d) { return y(d[0]); })
+                .y1(function (d) { return y(d[1]); });
+
+  var svg = d3.select("body").append("svg").attr("width","100%").attr("height","100%");
+  var chartGroup = svg.append("g").attr("transofrm", "translate("+margin.left+", "+margin.top+")");
+
+  var stacked = stack(xml);
+  console.log(stacked);
+
+
+
+
+
+
+
+
 });
