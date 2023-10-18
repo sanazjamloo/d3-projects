4e54fcadae7cc66f19abe607075ab31464e541b1
sanazjamloo <sanaz.jamloo@gmail.com>
2023-10-18 15:24:36 -0700

    add classes and styles

--- portfolio/d3-intro/selections/app.js
+++ portfolio/d3-intro/selections/app.js
@@ -1,8 +1,13 @@
 //chaining the methods
+// Any new attribute method will override the existing attribute methods. 
 const el = d3.select('body')
     .append('p')
-    .attr('class', 'foo')
-    .text('Hello World')
+    // .attr('class', 'foo')
+    // .attr('class', 'bar')
+    .classed('foo', true)
+    .classed('bar', true)
+    .text('Hello World!')
+    .style('color', 'blue')
 
 
 
