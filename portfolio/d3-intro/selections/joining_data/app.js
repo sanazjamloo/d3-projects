6d0bf78dc3232d200cf0fb0daeed38ec67972c22
sanazjamloo <sanaz.jamloo@gmail.com>
2023-10-21 13:38:29 -0700

    add enter function

--- portfolio/d3-intro/selections/joining_data/app.js
+++ portfolio/d3-intro/selections/joining_data/app.js
@@ -3,7 +3,10 @@ const data = [10, 20, 30, 40, 50]
 
 //creating a selection to join the data to.
 //using selectAll method because we have multiple li items.
-const el = d3.selectAll('li')
+const el = d3.select('ul')
+    .selectAll('li')
     .data(data)
-
+    .join('li')
+    .text('Hello!')
+//join method generates new elements, it doesnt add attributes or insert content into the existing elements.
 console.log(el)
