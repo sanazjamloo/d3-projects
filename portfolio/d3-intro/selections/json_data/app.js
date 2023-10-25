74fdbd9dd23117627d72419a95661a97902279fc
sanazjamloo <sanaz.jamloo@gmail.com>
2023-10-24 22:16:51 -0700

    add csv data

--- portfolio/d3-intro/selections/json_data/app.js
+++ portfolio/d3-intro/selections/json_data/app.js
@@ -1,5 +1,6 @@
 async function getData() {
-    const data = await d3.json('data.json')
+    //const data = await d3.json('data.json')
+    const data = await d3.csv(data.csv)
     console.log(data)
 }
 getData()
